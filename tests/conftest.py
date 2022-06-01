1405db567605c5f5f8c59dca3f307638dcb7e41c
I.M <=>
2022-06-01 12:00:09 -0300

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+    Dummy conftest.py for python_pushover_open_client.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    - https://docs.pytest.org/en/stable/fixture.html
+    - https://docs.pytest.org/en/stable/writing_plugins.html
+"""
+
+# import pytest
