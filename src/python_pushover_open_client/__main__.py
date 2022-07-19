fc93aa11e6d26c140fc5963c83cba657ab6dde53
I.M <=>
2022-07-19 17:57:45 -0300

    ok

--- src/python_pushover_open_client/__main__.py
+++ src/python_pushover_open_client/__main__.py
@@ -9,14 +9,13 @@ def cli():
 
 @cli.command()
 def json():
+
     @register_parser
     def return_json(raw_data):
         import json
         json_notification = json.dumps(raw_data)
 
         print(json_notification)
-
-        #return json_notification
 
     client = PushoverOpenClientRealTime()
     client.run_forever()
@@ -25,14 +24,4 @@ def json():
 if __name__ == "__main__":
 
     cli()
-
-    #pushover_open_client = PushoverOpenClient()
-    #pushover_open_client.login()
-    #pushover_open_client.register_device()
-    #pushover_open_client.download_messages()
-    #pushover_open_client.delete_all_messages()
-
-    #pushover_realtime_client =\
-            #    PushoverOpenClientRealTime(pushover_open_client=pushover_open_client)
-    #pushover_realtime_client.run_forever()
 
